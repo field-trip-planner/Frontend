f16e406d2e3bf5451772bed96fa64270b04286eb
stevekutz <stkutz@gmail.com>
2019-08-13 21:46:57 -0500

    initial

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h3> Running Now </h3>
+    </div>
+  );
+}
+
+export default App;
