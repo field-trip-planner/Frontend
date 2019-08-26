d55b943c91adedcb704a46feb5643f1df0068cde
seun-o <12373359+Seun-O@users.noreply.github.com>
2019-08-26 00:39:59 -0400

    Added parent registration form

--- src/Components/RegistrationModal/index.js
+++ src/Components/RegistrationModal/index.js
@@ -1,40 +1,41 @@
-import React from 'react';
-import { Button, Header, Modal, Grid, Icon } from 'semantic-ui-react';
-import SchoolLookupModal from '../SchoolLookupModal';
-import './registrationModal.css';
+import React from "react";
+import { Button, Header, Modal, Grid, Icon } from "semantic-ui-react";
+import SchoolLookupModal from "../SchoolLookupModal";
+import "./registrationModal.css";
+import ParentRegistrationModal from "../ParentRegistrationModal";
 
 const RegistrationModal = () => {
   return (
-
-    <Modal className="modal" size="large" trigger={<Button primary size='huge'>
-      Get Started
-  <Icon name='right arrow' />
-    </Button>}>
-      <Modal.Header className='modalHeader'>Registration</Modal.Header>
+    <Modal
+      className="modal"
+      size="large"
+      trigger={
+        <Button primary size="huge">
+          Get Started
+          <Icon name="right arrow" />
+        </Button>
+      }
+    >
+      <Modal.Header className="modalHeader">Registration</Modal.Header>
       <Modal.Content>
-
         <Grid className="gridContainer" columns={2}>
-
           <Grid.Column className="gridColumn">
-            <Header>
-              For Teachers
-        </Header>
+            <Header>For Teachers</Header>
             <Modal.Actions>
               <SchoolLookupModal />
             </Modal.Actions>
-
           </Grid.Column>
 
           <Grid.Column className="gridColumn">
-            <Header>
-              For Parents
-         </Header>
-            <Button>Sign Up</Button>
+            <Header>For Parents</Header>
+            <Modal.Actions>
+              <ParentRegistrationModal />
+            </Modal.Actions>
           </Grid.Column>
         </Grid>
-
       </Modal.Content>
-    </Modal>)
-}
+    </Modal>
+  );
+};
 
 export default RegistrationModal;
