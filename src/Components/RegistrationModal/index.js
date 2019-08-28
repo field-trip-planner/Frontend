6f73c54a6a722ff5b2218bc2453e025a2eb69265
seun-o <12373359+Seun-O@users.noreply.github.com>
2019-08-27 21:47:37 -0400

    added login modal to the main navigation

--- src/Components/RegistrationModal/index.js
+++ src/Components/RegistrationModal/index.js
@@ -1,20 +1,37 @@
-import React from "react";
-import { Button, Header, Modal, Grid, Icon } from "semantic-ui-react";
+import React, { useState } from "react";
+import { Header, Modal, Grid, Button, Icon } from "semantic-ui-react";
 import SchoolLookupModal from "../SchoolLookupModal";
 import "./registrationModal.css";
 import ParentRegistrationModal from "../ParentRegistrationModal";
 
-const RegistrationModal = () => {
+const RegistrationModal = ({ element }) => {
+  const [modalOpen, updateModalOpen] = useState(false);
+
+  const _handleOpen = e => {
+    updateModalOpen(true);
+  };
+
+  const _handleClose = e => {
+    updateModalOpen(false);
+  };
+
   return (
+    /*We want the open prop to have a false/neutral value before clicking on the trigger.
+    Once the trigger is clicked the value on the open prop will switch to true and the
+    modal will open. Once we click on the button to close the modal the value on the 
+    open prop will switch to false*/
+
     <Modal
       className="modal"
       size="large"
       trigger={
-        <Button primary size="huge">
+        <Button onClick={_handleOpen} primary size="huge">
           Get Started
           <Icon name="right arrow" />
         </Button>
       }
+      open={modalOpen}
+      close={_handleClose}
     >
       <Modal.Header className="modalHeader">Registration</Modal.Header>
       <Modal.Content>
@@ -22,7 +39,8 @@ const RegistrationModal = () => {
           <Grid.Column className="gridColumn">
             <Header>For Teachers</Header>
             <Modal.Actions>
-              <SchoolLookupModal />
+              {/* <button onClick={_handleClose}></button> */}
+              <SchoolLookupModal test={_handleClose} />
             </Modal.Actions>
           </Grid.Column>
 
--- src/Components/SchoolLookupModal/index.js
+++ src/Components/SchoolLookupModal/index.js
@@ -1,23 +1,29 @@
-import React from 'react';
-import { Button, Modal, Container, Input } from 'semantic-ui-react';
-import SchoolRegistrationModal from '../SchoolRegistrationModal';
-import './schoolLookupModal.css';
+import React from "react";
+import { Button, Modal, Container, Input } from "semantic-ui-react";
+import SchoolRegistrationModal from "../SchoolRegistrationModal";
+import "./schoolLookupModal.css";
 
-
-
-export default () => {
+export default props => {
   return (
     <>
-
-      <Modal className="modal" size="large" trigger={<Button>
-        Sign Up
-    </Button>}>
-        <Modal.Header className='modalHeader'>School Look Up</Modal.Header>
+      <Modal
+        className="modal"
+        size="large"
+        trigger={<Button onClick={props.test}>Sign Up</Button>}
+      >
+        <Modal.Header className="modalHeader">School Look Up</Modal.Header>
         <Modal.Content>
           <Container className="contentContainer" textAlign="center">
-            <Input className="schoolLookUpSearchBar" size="large" icon='building' iconPosition='left' placeholder='Search Schools...' />
+            <Input
+              className="schoolLookUpSearchBar"
+              size="large"
+              icon="building"
+              iconPosition="left"
+              placeholder="Search Schools..."
+            />
 
-            <p>Don't see your school?
+            <p>
+              Don't see your school?
               <SchoolRegistrationModal />
             </p>
 
@@ -25,7 +31,6 @@ export default () => {
           </Container>
         </Modal.Content>
       </Modal>
-
     </>
-  )
-}
+  );
+};
