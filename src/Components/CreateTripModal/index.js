072f61b6782c9bcf78e91e734436fb29576ffa04
Seun <12373359+NatureBoi@users.noreply.github.com>
2019-09-13 21:01:44 -0400

    Made changes to api endpoints and few changes to register api call

--- src/Components/CreateTripModal/index.js
+++ src/Components/CreateTripModal/index.js
@@ -1,20 +1,20 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import api from "../../api";
 import { Container, Button, Modal, Form, Icon } from "semantic-ui-react";
 
-const CreateTripModal= (props) => {
-  const [ fieldTripInfo, setfieldTripInfo ] = useState({
+const CreateTripModal = props => {
+  const [fieldTripInfo, setfieldTripInfo] = useState({
     name: "",
     date: "",
     address: "",
     supplies: "",
     cost: "",
-    field_trip_details: "",
+    field_trip_details: ""
   });
 
   const _handleChange = e => {
     const { name, value } = e.target;
-    console.log('input enter',value)
+    console.log("input enter", value);
     setfieldTripInfo({
       ...fieldTripInfo,
       [name]: value
@@ -23,34 +23,36 @@ const CreateTripModal= (props) => {
 
   const _handleSubmit = e => {
     e.preventDefault();
-      api
-        .post("fieldtrips",fieldTripInfo)
-        .then(({ data }) => {
-          setfieldTripInfo({
-            name: "",
-            date: "",
-            address: "",
-            supplies: "",
-            cost: "",
-            school_id: "4187269f-d1fa-41fe-ad34-2e7d74a9031a",
-            creator_id: "59495f61-f31c-444d-a284-b2233e5aa914",
-            field_trip_details: "",
-          })
-          props.onSubmitSuccess();
-          return data;
-        })
-        .catch(err => err);
+    api
+      .post("fieldtrips", fieldTripInfo)
+      .then(({ data }) => {
+        setfieldTripInfo({
+          name: "",
+          date: "",
+          address: "",
+          supplies: "",
+          cost: "",
+          school_id: "4187269f-d1fa-41fe-ad34-2e7d74a9031a",
+          creator_id: "59495f61-f31c-444d-a284-b2233e5aa914",
+          field_trip_details: ""
+        });
+        props.onSubmitSuccess();
+        return data;
+      })
+      .catch(err => err);
     console.log(fieldTripInfo);
   };
 
-    
   return (
     <>
-      <Modal trigger={
-      <Button floated="right" primary>
-        <Icon name="add" />
-      Create Trip
-    </Button>}>
+      <Modal
+        trigger={
+          <Button floated="right" primary>
+            <Icon name="add" />
+            Create Trip
+          </Button>
+        }
+      >
         <Modal.Header className="modalHeader">Create Your Trip!</Modal.Header>
         <Modal.Content>
           <Container>
@@ -71,6 +73,7 @@ const CreateTripModal= (props) => {
                   name="date"
                   value={fieldTripInfo.date}
                   onChange={_handleChange}
+                  placeholder="MM/DD/YYYY"
                 />
                 <Form.Input
                   fluid
