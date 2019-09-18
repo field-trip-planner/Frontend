4cb62acdd48dda98a006b2f3987660b82e43a42e
stevekutz <stkutz@gmail.com>
2019-09-17 22:21:38 -0500

    reverted for dev

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,10 @@
 import axios from "axios";
+const baseURLvar = process.env.REACT_APP_URL
+
 export default axios.create({
-  baseURL: `https://calm-spire-14878.herokuapp.com/`,
+  baseURL: baseURLvar,
+  //baseURL: "http://localhost:5000",
+  //baseURL: `https://calm-spire-14878.herokuapp.com/`,
   withCredentials: true,
   headers: {
     "Content-Type": "application/json"
