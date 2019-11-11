af163b9a00227da3f1468789585342ee83cace7f
LD <LDinho@users.noreply.github.com>
2019-11-10 23:02:48 -0500

    update landing page
    - and remove the about us for now

--- src/Components/LandingPage/index.js
+++ src/Components/LandingPage/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
+import MainContent from './MainContent';
 
 export default () => {
   return (
     <>
       <Header />
+      <MainContent />
       <Footer />
     </>
   )
